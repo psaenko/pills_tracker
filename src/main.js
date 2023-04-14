0b5b36bd1212f84fde881be683c2a88582bf4cc7
Pavel <saenko359@gmail.com>
2023-04-14 14:45:19 +0300

    test 1

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
 
-createApp(App).use(store).use(router).mount("#app");
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+createApp(App)
+    .use(store)
+    .use(router)
+    .use(vuetify)
+    .mount('#app')
